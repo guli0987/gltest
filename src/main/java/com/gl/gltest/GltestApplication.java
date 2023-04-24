f0ed95525be4e6d1f87735a27cc5c1ce2f979adf
testa <test_a@foxmail.com>
2023-04-24 21:18:02 +0800

    init

---
+++ src/main/java/com/gl/gltest/GltestApplication.java
@@ -0,0 +1,13 @@
+package com.gl.gltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GltestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GltestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gl/gltest/GltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.gltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GltestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
